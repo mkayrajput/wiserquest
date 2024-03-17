155453d1e4af88a11b17805423cc425f8b66a96a
devhub-mk <mahender.codedrill@gmail.com>
2024-03-17 19:07:05 +0530

    Theme setup completed

---
+++ components/ManageProfileMenu.tsx
@@ -0,0 +1,52 @@
+import { Github, LifeBuoy, LogOut, Settings, User } from 'lucide-react'
+
+import UserAvtar from './UserAvtar'
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu'
+
+export default function ManageProfileMenu() {
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <UserAvtar />
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className='w-56'>
+        <DropdownMenuLabel>My Account</DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuGroup>
+          <DropdownMenuItem>
+            <User className='mr-2 h-4 w-4' />
+            <span>Profile</span>
+          </DropdownMenuItem>
+          <DropdownMenuItem>
+            <Settings className='mr-2 h-4 w-4' />
+            <span>Settings</span>
+          </DropdownMenuItem>
+        </DropdownMenuGroup>
+        <DropdownMenuSeparator />
+        <DropdownMenuGroup></DropdownMenuGroup>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem>
+          <Github className='mr-2 h-4 w-4' />
+          <span>GitHub</span>
+        </DropdownMenuItem>
+        <DropdownMenuItem>
+          <LifeBuoy className='mr-2 h-4 w-4' />
+          <span>Support</span>
+        </DropdownMenuItem>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem>
+          <LogOut className='mr-2 h-4 w-4' />
+          <span>Log out</span>
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  )
+}
---
+++ components/UserAvtar.tsx
@@ -0,0 +1,15 @@
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
+import { cn } from '@/lib/utils'
+
+export default function UserAvtar() {
+  return (
+    <Avatar>
+      <AvatarImage
+        src='https://github.com/shadcn.png'
+        alt='@shadcn'
+        className={cn('cursor-pointer')}
+      />
+      <AvatarFallback>CN</AvatarFallback>
+    </Avatar>
+  )
+}
