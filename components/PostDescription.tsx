9e4d4307a4dcb59a63a93ef2b31c112076e5c718
devhub-mk <mahender.codedrill@gmail.com>
2024-03-19 17:42:42 +0530

    Single quotes removed

--- components/PostDescription.tsx
+++ components/PostDescription.tsx
@@ -1,6 +1,6 @@
-import { Button } from '@/components/ui/button'
-import { Input } from '@/components/ui/input'
-import { Label } from '@/components/ui/label'
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 import {
   Sheet,
   SheetClose,
@@ -10,13 +10,13 @@ import {
   SheetHeader,
   SheetTitle,
   SheetTrigger,
-} from '@/components/ui/sheet'
+} from "@/components/ui/sheet";
 
 export function PostDescription() {
   return (
     <Sheet>
       <SheetTrigger asChild>
-        <Button variant='outline'>Open</Button>
+        <Button variant="outline">Open</Button>
       </SheetTrigger>
       <SheetContent>
         <SheetHeader>
@@ -25,26 +25,26 @@ export function PostDescription() {
             Make changes to your profile here. Click save when you{"'"}re done.
           </SheetDescription>
         </SheetHeader>
-        <div className='grid gap-4 py-4'>
-          <div className='grid grid-cols-4 items-center gap-4'>
-            <Label htmlFor='name' className='text-right'>
+        <div className="grid gap-4 py-4">
+          <div className="grid grid-cols-4 items-center gap-4">
+            <Label htmlFor="name" className="text-right">
               Name
             </Label>
-            <Input id='name' value='Pedro Duarte' className='col-span-3' />
+            <Input id="name" value="Pedro Duarte" className="col-span-3" />
           </div>
-          <div className='grid grid-cols-4 items-center gap-4'>
-            <Label htmlFor='username' className='text-right'>
+          <div className="grid grid-cols-4 items-center gap-4">
+            <Label htmlFor="username" className="text-right">
               Username
             </Label>
-            <Input id='username' value='@peduarte' className='col-span-3' />
+            <Input id="username" value="@peduarte" className="col-span-3" />
           </div>
         </div>
         <SheetFooter>
           <SheetClose asChild>
-            <Button type='submit'>Save changes</Button>
+            <Button type="submit">Save changes</Button>
           </SheetClose>
         </SheetFooter>
       </SheetContent>
     </Sheet>
-  )
+  );
 }
