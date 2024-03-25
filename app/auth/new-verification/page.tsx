73926835223451457c6eced0ee9166bbc7f59561
devhub-mk <mahender.codedrill@gmail.com>
2024-03-25 14:45:33 +0530

    New user email verification complete

---
+++ app/auth/new-verification/page.tsx
@@ -0,0 +1,5 @@
+import { NewVerificationForm } from "@/components/auth/new-verification-from";
+
+export default function NewVerificationPage() {
+  return <NewVerificationForm />;
+}
--- components/auth/login-form.tsx
+++ components/auth/login-form.tsx
@@ -49,8 +49,7 @@ export const LoginForm = () => {
     startTransition(() => {
       login(values).then((data) => {
         setError(data?.error);
-        //TODO: ADD 2FA
-        // setSuccess(data?.success);
+        setSuccess(data?.success);
       });
     });
   };
